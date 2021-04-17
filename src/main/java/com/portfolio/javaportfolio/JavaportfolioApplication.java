67933b26bdd60cf9f23f4461636f1ba98af9c13d
Rod-S <rodsanati@gmail.com>
2021-04-16 23:18:59 -0500

    first commit, spring initializr web setup

---
+++ src/main/java/com/portfolio/javaportfolio/JavaportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.javaportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaportfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaportfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/javaportfolio/JavaportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.javaportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaportfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
