74cdb7f8bd2af722d6525c99dcdbf27685885f9f
Rod-S <rodsanati@gmail.com>
2021-06-17 08:28:48 -0500

    fix controller and layout template

---
+++ src/main/java/com/portfolio/javaportfolio/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.portfolio.javaportfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    public HomeController() {};
+
+    @GetMapping("/")
+    public String showHome() {
+        return "portfolio/layout";
+    }
+}
